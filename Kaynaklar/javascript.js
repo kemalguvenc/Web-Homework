30c302601b9d68764767e4a0cc820592a1a739a3
Muhammet Kemal Güvenç <63072338+kemalguvenc@users.noreply.github.com>
2020-05-05 08:13:04 +0300

    Hakkımda Sayfası
    Hakkımda sayfası ve kaynaklar klasörü eklendi.

---
+++ Kaynaklar/javascript.js
@@ -0,0 +1,8 @@
+var texts= ["Dişbudak ağaçlarıyla çevrili ve şehrin stresinden uzak büyüleyici bir yer.",
+            "Restoran ve kafeteryanın da hizmet verdiği tesislerde şehrin gizli kalmış güzelliğini keşfedebilirsin.",
+            "Kocaala ve Karasu ilçelerinin arasında akan Maden Deresi, etrafını saran ağaçlar ve şelalesiyle doğaseverleri adeta büyülüyor.",
+            "Geyve Maksudiye'de bulunan Doğançay Şelalesi tabiat anıtında doğal güzelliklerin keyfini çıkarabilirsin."];
+
+function print(i){
+        document.getElementById("descriptation-photo").innerHTML="<br/><br/><br/><br/><br/><br/>"+texts[i]+"</p>"
+}
